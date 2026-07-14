22d09021e492a5c77653ebe31e17bcda6f7d4309
hazelnutflavoured <hazelkwon.00@gmail.com>
2026-07-14 16:25:20 +0900

    Add app/main

---
+++ app/apis/practice_apis.py
@@ -0,0 +1,34 @@
+import re
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel, field_validator, ConfigDict
+from typing import Optional
+
+# Initialize router
+router = APIRouter(prefix="/practice_api", tags=["practice"])
+
+# User Database
+user_list = [
+    {
+        "id": 1,
+        "name": "홍길동",
+        "age": 24,
+        "email": "gildong24@example.com",
+        "password": "Password1234!!"
+    },
+    {
+        "id": 2,
+        "name": "장문복",
+        "age": 21,
+        "email": "moonluck12@example.com",
+        "password": "Check1321!"
+    },
+    {
+        "id": 3,
+        "name": "임우진",
+        "age": 31,
+        "email": "limousine33@example.com",
+        "password": "lwsPAssword12@"
+    }
+]
+
+
