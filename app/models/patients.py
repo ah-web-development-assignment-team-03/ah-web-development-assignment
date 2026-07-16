f2f7f5fd7139a325a0d7a5ec4cc6309735b8b412
Funital <gksrmawns0925@naver.com>
2026-07-16 11:39:15 +0900

    [#12] sqlite 환경을 고려해 기본키 타입 수정
    #12

--- app/models/patients.py
+++ app/models/patients.py
@@ -1,7 +1,7 @@
 import enum
 from datetime import datetime
 
-from sqlalchemy import Integer, String, DateTime, Enum, func
+from sqlalchemy import BigInteger, SmallInteger, Integer, String, DateTime, Enum, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db.databases import Base
@@ -15,9 +15,9 @@ class GenderEnum(enum.Enum):
 class Patient(Base):
     __tablename__ = "patients"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(30), nullable=False, comment="환자 성명")
-    age: Mapped[int] = mapped_column(Integer, nullable=False, comment="환자 나이")
+    age: Mapped[int] = mapped_column(SmallInteger().with_variant(Integer, "sqlite"), nullable=False, comment="환자 나이")
     gender: Mapped[GenderEnum | None] = mapped_column(Enum(GenderEnum, native_enum=False), nullable=True, comment="환자 성별")
     phone: Mapped[str] = mapped_column(String(11), nullable=False, comment="환자 연락처, 국내 전화번호로 한정")
     created_at: Mapped[datetime] = mapped_column(
